fdb9b3ce7ce881637ed036c978f6c6ecb9510b94
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-06-11 08:03:39 +0200

    test on files built by createLexicon task, not static resources

--- marytts-languages/src/test/groovy/marytts/language/LTSTest.groovy
+++ marytts-languages/src/test/groovy/marytts/language/LTSTest.groovy
@@ -21,17 +21,19 @@ class LTSTest {
         allophoneSetFile = new File(allophoneSetPath)
         lexiconFile = new File(lexiconPath)
         def allophoneSet = AllophoneSet.getAllophoneSet(allophoneSetFile.newInputStream(), 'test')
-        def ltsStream = getClass().getResourceAsStream(ltsResourcePath)
+        def ltsStream = new File(ltsResourcePath).newInputStream()
         lts = new TrainedLTS(allophoneSet, ltsStream, false);
-        def fstStream = getClass().getResourceAsStream(fstResourcePath)
+        def fstStream = new File(fstResourcePath).newInputStream()
         fst = new FSTLookup(fstStream, null);
     }
 
     @Test
-    @Parameters(['allophoneset', 'lexicon'])
-    void canReadResources(String allophoneSetPath, String lexiconPath) {
+    @Parameters(['allophoneset', 'lexicon', 'lts', 'fst'])
+    void canReadResources(String allophoneSetPath, String lexiconPath, String ltsResourcePath, String fstResourcePath) {
         FileAssert.assertReadable(allophoneSetFile)
         FileAssert.assertReadable(lexiconFile)
+        FileAssert.assertReadable(new File(ltsResourcePath))
+        FileAssert.assertReadable(new File(fstResourcePath))
     }
 
     Collection parseLexicon() {
