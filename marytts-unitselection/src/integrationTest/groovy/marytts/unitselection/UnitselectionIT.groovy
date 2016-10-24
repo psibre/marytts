b30e039b9917aa7237b4645dce738e66cc6d2e53
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-10-24 10:20:56 +0200

    tweak IT

--- marytts-unitselection/src/integrationTest/groovy/marytts/unitselection/UnitselectionIT.groovy
+++ marytts-unitselection/src/integrationTest/groovy/marytts/unitselection/UnitselectionIT.groovy
@@ -1,16 +1,24 @@
 package marytts.unitselection
 
+import groovy.util.logging.Log4j
+
 import marytts.server.MaryProperties
+import marytts.util.MaryRuntimeUtils
 
 import org.testng.annotations.*
 
+@Log4j
 class UnitselectionIT {
+
+    @BeforeClass
+    void setUp() {
+        MaryRuntimeUtils.ensureMaryStarted()
+    }
 
     @Test
     void testProperty() {
         def synthesizers = MaryProperties.getList("synthesizers.classes.list")
-        assert synthesizers
-        assert synthesizers.contains("marytts.unitselection.UnitSelectionSynthesizer")
+        assert synthesizers?.contains("marytts.unitselection.UnitSelectionSynthesizer")
     }
 
 }
